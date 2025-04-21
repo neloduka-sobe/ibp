b7b5397133418bd0ba101dbd04ba2f18e17e053c
neloduka-sobe <nelodukasobe@gmail.com>
2025-04-21 12:18:27 +0200

    Auto-deploy from Next.js build

---
+++ _next/static/chunks/app/page-aedc23a728ba96ca.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{5174:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,8003,23))}},function(e){e.O(0,[756,971,117,744],function(){return e(e.s=5174)}),_N_E=e.O()}]);
